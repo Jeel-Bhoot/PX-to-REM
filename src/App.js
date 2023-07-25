2e3f985739385443f93d84f478e74cd71ae8f727
Abhi Shobhashana <abhishob9099@gmail.com>
2023-07-26 02:17:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
         </Helmet>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
